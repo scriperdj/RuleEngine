fc8473373794d99f8d0f88a732a1319ae051adef
sathish <scriperdj@gmail.com>
2017-06-15 00:01:35 +0530

    added logical statements

--- rule_engine.py
+++ rule_engine.py
@@ -1,6 +1,62 @@
+import json
+import operator
+from datetime import datetime
 
-def get_string():
-    return "ok"
+OPERATORS = {
+    "eq": operator.eq,
+    "ne": operator.ne,
+    "lt": operator.lt,
+    "gt": operator.gt,
+    "le": operator.le,
+    "ge": operator.ge
+}
+
+# Load rules in memory
+with open("rules.json") as data_file:
+    RULES = json.load(data_file)
+
+def get_rule(key):
+    try:
+        return RULES[key]
+    except:
+        return ''
+
+# Check if the record is valid as per rules
+def check_validity(rec):
+    valid = True
+    rule = get_rule(rec["signal"] + "_" + rec["value_type"])
+
+    if rule:
+        opr = OPERATORS[rule["cond"]]
+        if rec["value_type"] == "Datetime":
+            left = datetime.now() if rule["value"] == "current" else datetime.strptime(rule["value"], '%Y-%m-%d %H:%M:%S')
+            right = datetime.strptime(rec["value"], '%Y-%m-%d %H:%M:%S')
+            valid = opr(left,right)
+        elif rec["value_type"] == "Integer":
+            valid = opr(float(rule["value"]),float(rec["value"]))
+        else:
+            valid = opr(str(rule["value"]),str(rec["value"]))
+
+    return valid
+
+def check_date_rules(rec):
+    valid = True
+    return valid
 
 if __name__ == "__main__":
-    print("Starting Rule Engine..")
+    print("Executing Rules Engine..\n\n")
+
+    # Read raw data from input file
+    raw_data=[]
+    with open("raw_data.json") as raw_file:
+        raw_data = json.load(raw_file)
+
+    # Loop through data & check if it violates any rules
+    print("Below are data signals which violates rules.\n")
+    for rec in raw_data:
+        is_valid = check_validity(rec)
+
+        if not is_valid:
+            print(rec)
+
+    print("\n\nEnd of execution..")
--- tests/test_rule_engine.py
+++ tests/test_rule_engine.py
@@ -1,10 +1,11 @@
 import unittest
-from rule_engine import get_string
+from rule_engine import load_rules
 
 class TestRuleEngine(unittest.TestCase):
 
-    def test_upper(self):
-        self.assertEqual(get_string(), 'ok')
+    def test_load_rules(self):
+        rules = load_rules('rules.json')
+        self.assertEqual(type(rules), dict)
 
 if __name__ == '__main__':
     unittest.main()
